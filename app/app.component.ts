7f0944da5712475b87d4f90900f8c2ec044dab6e
Remco Weekers <remco@craftsmen.nl>
2016-09-02 22:09:11 +0200

    Add subcomponent.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
+import { SongsComponent } from './songs.component';
+
 @Component({
   selector: 'my-app',
-  template: '<h1>Voornamen in liedjes</h1>'
+  template: `<h1>Voornamen in liedjes</h1>
+            <songs></songs>`,
+  directives: [SongsComponent]
 })
 export class AppComponent { }
---
+++ app/songs.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'songs',
+  templateUrl: 'app/songs.component.html',
+  styleUrls:['app/songs.component.css']
+})
+export class SongsComponent {
+
+}
