d1154892c0e9225d9f984b2600464676e62936ad
Remco Weekers <remco@craftsmen.nl>
2016-09-03 13:47:29 +0200

    Add mock for songs.

--- app/songs.component.ts
+++ app/songs.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Song } from './song';
+import { SONGS } from './mocks';
 
 @Component({
   selector: 'songs',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls:['app/songs.component.css']
 })
 export class SongsComponent {
+  songs: Song[];
 
+  ngOnInit() {
+    this.songs = SONGS;
+  }
 }
