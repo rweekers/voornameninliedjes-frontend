1c8b681147a8613a401ff95541fef2c0dfa38145
Remco Weekers <remco@craftsmen.nl>
2016-09-19 23:08:23 +0200

    Add pagination (client-side)

--- src/client/app/home/home.module.ts
+++ src/client/app/home/home.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { HomeComponent } from './home.component';
 import { SongListService } from '../shared/song-list/index';
+import {Ng2PaginationModule} from 'ng2-pagination';
 
 @NgModule({
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule, Ng2PaginationModule],
   declarations: [HomeComponent],
   exports: [HomeComponent],
   providers: [SongListService]
