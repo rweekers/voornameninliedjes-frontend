0cd18df0b6d8a1109592efe1a535c5bafa0ca5bc
Remco Weekers <remco@craftsmen.nl>
2017-01-21 09:16:50 +0100

    Add path to java backend

--- tools/env/test.ts
+++ tools/env/test.ts
@@ -3,6 +3,7 @@ import {EnvConfig} from './env-config.interface';
 const TestConfig: EnvConfig = {
   ENV: 'TEST',
   API: 'http://localhost:3000/api/n/songs'
+  // API: 'http://localhost:8080/namesandsongs/api/song'
 };
 
 export = TestConfig;
