c9552d009fba09b7bf33df6251722402cf9c2327
Remco Weekers <Remco.Weekers@gmail.com>
2017-02-06 14:17:17 +0100

    Update home.component.e2e-spec.ts

--- src/client/app/home/home.component.e2e-spec.ts
+++ src/client/app/home/home.component.e2e-spec.ts
@@ -4,21 +4,9 @@ describe('Home', () => {
     browser.get('/');
   });
 
-  it('should have an input', () => {
-    expect(element(by.css('sd-home form input')).isPresent()).toEqual(true);
-  });
-
-  it('should have a list of computer scientists', () => {
+  it('should have a list of songs', () => {
     expect(element(by.css('sd-home ul')).getText())
-      .toEqual('Edsger Dijkstra\nDonald Knuth\nAlan Turing\nGrace Hopper');
-  });
-
-  it('should add a name to the list using the form', () => {
-    element(by.css('sd-home form input')).sendKeys('Tim Berners-Lee');
-    element(by.css('sd-home form button')).click();
-
-    expect(element(by.css('sd-home ul')).getText())
-      .toEqual('Edsger Dijkstra\nDonald Knuth\nAlan Turing\nGrace Hopper\nTim Berners-Lee');
+      .toEqual('Sandra Lauer - (I''ll never be) Maria Magdalena\nElvis Presley - (Marie''s the name) his latest flame\nHarry Vermeegen - 1-2-3-4 Dennis bier');
   });
 
 });
