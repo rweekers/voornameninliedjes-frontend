73b36673852012748e000388cd6b93a29e34dc41
Remco Weekers <Remco.Weekers@gmail.com>
2017-02-06 10:06:37 +0100

    Remove unused import

--- src/client/app/home/home.component.ts
+++ src/client/app/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { SongListService, Song } from '../shared/index';
-import { Observable } from 'rxjs/Observable';
 
 /**
  * This class represents the lazy loaded HomeComponent.
