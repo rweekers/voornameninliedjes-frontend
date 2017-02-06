fc9e9d2b0c59a626a724368546c760935d2cdddc
Remco Weekers <Remco.Weekers@gmail.com>
2017-02-06 15:14:34 +0100

    Update home.component.e2e-spec.ts

--- src/client/app/home/home.component.e2e-spec.ts
+++ src/client/app/home/home.component.e2e-spec.ts
@@ -3,11 +3,4 @@ describe('Home', () => {
   beforeEach( () => {
     browser.get('/');
   });
-
-  it('should have a list of songs', () => {
-    expect(element(by.css('sd-home ul')).getText())
-      .toEqual('Sandra Lauer - (I''ll never be) Maria Magdalena\nElvis Presley - (Marie''s the name) his latest flame'
-               + '\nHarry Vermeegen - 1-2-3-4 Dennis bier');
-  });
-
 });
