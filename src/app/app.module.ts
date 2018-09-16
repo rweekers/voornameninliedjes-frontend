d2e662ba3d2a4589feec21d54b0c832cb77021d9
Remco Weekers <remco@craftsmen.nl>
2018-09-16 22:49:43 +0200

    Add mat card for showing list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatGridListModule, MatCardModule } from '@angular/material';
+
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
 
@@ -15,7 +18,12 @@ import { SongsComponent } from './songs/songs.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    NoopAnimationsModule,
+    MatButtonModule, 
+    MatCheckboxModule,
+    MatGridListModule,
+    MatCardModule
     // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
     // and returns simulated server responses.
     // Remove it when a real server is ready to receive requests.
