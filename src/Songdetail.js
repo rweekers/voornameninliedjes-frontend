9f5a5f9e66bd165317d251749c8889534fb237d5
Remco Weekers <rweekers@bol.com>
2019-03-04 13:24:41 +0100

    Update songdetail

--- src/Songdetail.js
+++ src/Songdetail.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
 import './Songdetail.css';
 
-class Songdetail extends Component {
-  render() {
-    const song = this.props;
+const API = 'https://api.voornameninliedjes.nl/songs/';
 
-    console.log('Opened detail!')
+class Songdetail extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      song: ''
+    };
+  }
+
+  componentDidMount() {
+    this.id = this.props.match.params.id;
+
+    fetch(API + this.id)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ song: data });
+      });
+  }
+
+  render() {
+    const song = this.state.song;
 
     return (
       <div className="Songdetail">
-        <p>{song.artist} ---- {song.title}</p>
+        <p>{song.artist} - {song.title}</p>
+        <p>{song.background}</p>
       </div>
     );
   }
